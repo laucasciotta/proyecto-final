8205aed272892451d79c4036817bbabf0d094d66
laucasciotta <casciotta15@gmail.com>
2021-10-07 21:34:58 -0300

    agrego rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
+import { Container, Spinner } from 'react-bootstrap';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import  NavRB  from './components/NavRB';
+import Bebidas from './pages/Bebidas';
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import Perfil from './pages/Perfil';
+import { useEffect, useState } from 'react';
+import DetalleProducto from './pages/DetalleProducto';
+import Register from './pages/Register';
+import Contacto from './pages/Contacto';
 
 function App() {
-  return (
-    <h1>Proyecto final </h1>
-  );
+  const [bebidas, setBebidas] = useState([])
+  const [user, setUser] = useState({})
+    return (
+        <div className="footer-fix">
+            <NavRB user={user} />
+
+            <Container>
+                <Switch>
+                    <Route path="/" exact>
+                        <Bebidas bebidas={bebidas} />
+              
+                    </Route>
+
+                    <Route path="/login">
+                        <Login  />
+                    </Route>
+
+                    {/* {isAdmin && ( */}
+                        <Route path="/admin">
+                            <Admin  />
+                        </Route>
+                    {/* )} */}
+
+                   
+                        <Route path="/perfil">
+                            <Perfil />
+                        </Route>
+                 
+
+                    <Route path="/producto/:productoId">
+                        <DetalleProducto />
+                    </Route>
+
+                    <Route path="/register">
+                        <Register />
+                    </Route>
+
+                    <Route path="/404">404</Route>
+                    <Route path="/contacto">
+                        <Contacto />
+                    </Route>
+
+                    <Route path="*">
+                        <Redirect to="/404" />
+                    </Route>
+                    
+                </Switch>
+            </Container>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/DetalleProducto.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetalleProducto() {
+    return (
+        <div>
+            detalle DetalleProducto
+        </div>
+    )
+}
