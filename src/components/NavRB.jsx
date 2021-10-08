90bafbcacb700741cd6df2f73ea0d2d5c48d8fda
laucasciotta <casciotta15@gmail.com>
2021-10-07 21:58:00 -0300

    nav lista

--- src/components/NavRB.jsx
+++ src/components/NavRB.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
-export default function NavRB() {
-    return (
-        <div>
-            navvvvvvvvvv
-        </div>
-    )
+export default function NavRB({ user }) {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/" exact activeClassName="text-white">
+              Memes
+            </Nav.Link>
+
+            {/* {!tokenLocal.token && ( */}
+              <Nav.Link
+                as={NavLink}
+                to="/login"
+                exact
+                activeClassName="text-white"
+              >
+                Login
+              </Nav.Link>
+                 {/* )} */}
+
+            <Nav.Link
+              as={NavLink}
+              to="/perfil"
+              exact
+              activeClassName="text-white"
+            >
+              {" "}
+              Perfil
+            </Nav.Link>
+
+            {/* {user.role === "admin" && ( */}
+              <Nav.Link
+                as={NavLink}
+                to="/admin"
+                exact
+                activeClassName="text-white"
+              >
+               subir bebida
+              </Nav.Link>
+            {/* )} */}
+
+           
+              <Nav.Link
+                as={NavLink}
+                to="/Register"
+                exact
+                activeClassName="text-white"
+              >
+                Registro
+              </Nav.Link>
+      
+
+         <Button >Logout</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
